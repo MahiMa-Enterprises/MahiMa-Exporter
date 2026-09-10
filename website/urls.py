318f069d306139c2e9b3c2671c27726e3571b2b6
Ruturaj Bhunje <ruturajbhunje1616@gmail.com>
2026-09-10 16:38:01 +0530

    Add robots.txt

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 
@@ -45,6 +47,16 @@ urlpatterns = [
         "googlef24fe2e733aa6bfa.html",
         views.google_verification,
         name="google_verification"
+    ),
+
+    # Robots.txt
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain"
+        ),
+        name="robots_txt"
     ),
 
 ]
