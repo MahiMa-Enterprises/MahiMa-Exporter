6549668e668fcdbbb5ea647942788d509924a0cf
Ruturaj Bhunje <ruturajbhunje1616@gmail.com>
2026-09-10 15:51:31 +0530

    Add XML sitemap

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+
+from website.sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+
     path("", include("website.urls")),
 ]
---
+++ website/sitemaps.py
@@ -0,0 +1,20 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.8
+    changefreq = "weekly"
+
+    def items(self):
+        return [
+            "home",
+            "about",
+            "products",
+            "affiliation",
+            "enquiry",
+            "contact",
+        ]
+
+    def location(self, item):
+        return reverse(item)
